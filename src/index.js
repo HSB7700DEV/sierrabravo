c1cca3e22fc699bfe5fd3a5772a79e9ee3f1337a
thshsb <hssdbrv@gmail.com>
2025-11-07 23:50:18 +0330

    removed scripts and any modifications for new api

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import startCommand from './commands/start';
 import pingCommand from './commands/ping';
 import currencyprizeCommand from './commands/currencyprize';
 import html from '/main/index.html';
-import getGoldAndDollarRates from './goldapi';
 
 // A Map to store our command handlers for easy lookup
 const commands = new Map();
@@ -62,14 +61,7 @@ export default {
             }
         }
     }
-
-    if (url.pathname === "/gold") {
-      const data = await getGoldAndDollarRates();
-      return new Response(JSON.stringify(data, null, 2), {
-        headers: { "Content-Type": "application/json" },
-      });
-    }
-
+    
     // Handle Telegram webhooks
     if (request.method === "POST") {
       const payload = await request.json();
